eff61c3cfd89a517fbc746500d9001bd14ff3f12
veramocor <veramocor.v@gmail.com>
2022-02-22 11:59:02 +0900

    Refactor Player, GameManager using Singleton

--- Assets/Scripts/Manager Group/GameManager.cs
+++ Assets/Scripts/Manager Group/GameManager.cs
@@ -1,20 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using BAV.Common;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
-    public Player player;
-    public PlayerMovement playerMovement;
-    public TalkManager talkManager;
-    public QuestManager questManager;
+	// Methods --------------------------------------------------------------------------------------
+	public void Action(ObjectData obj)
+	{
+		player.Movement.Stop();
+		talkManager.Talk(questManager.curQuestNum, obj);
+	}
 
-    public void Action(ObjectData obj)
-    {
-        playerMovement.Stop();
-        talkManager.Talk(questManager.curQuestNum, obj);
 
-    }
 
+	// Fields : caching -----------------------------------------------------------------------------
+	private Player player;
+
+
+
+	// Unity Inspectors -----------------------------------------------------------------------------
+	public TalkManager talkManager;
+	public QuestManager questManager;
+
+	// Unity Messages -------------------------------------------------------------------------------
+	private void Awake()
+	{
+		// player가 1개이고, GameManager 생성전에 이미 존재할 경우만 가능
+		// 다른 대안으로 PlayerManager를 따로 둘 수도 있고, GameManager가 생성시킬 수도 있음
+		// 2022.02.22 by veramocor
+		player = FindObjectOfType<Player>();
+	}
 }
- 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,200 +6,191 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    public GameManager manager;
+	// Properties -----------------------------------------------------------------------------------
+	public PlayerMovement Movement => playerMovement;
+	public bool IsPowerFull => curHealth >= maxHealth;
+	public bool IsShieldFull => curShieldPower >= maxShield;
+	public float Coin => coin;
+	public int SwordNum => swordNum;
 
-    Camera cam;
-    Animator anim;
-    Rigidbody rigid;
-    [SerializeField]
-    GameObject nearObj;//상호작용 가능한 object
-    PlayerMovement playerMovement;
+	// Methods --------------------------------------------------------------------------------------
+	public void ResumePlayer()
+	{
+		playerMovement.canMove = true;
+	}
+	public void StopPlayer()
+	{
+		playerMovement.canMove = false;
+	}
+	public void ChangeCoin(int val)
+	{
+		coin += val;
+		coinText.text = "X " + coin;
+	}
+	public void ChangeHealth(int val)
+	{
+		curHealth += val;
+		if (curHealth > maxHealth)
+			curHealth = maxHealth;
 
-    [Header("Player Power")]
-    public float maxHealth = 100;
-    public int maxSword = 10;
-    public int maxShield = 10;
+		healthBar.localScale = new Vector3((float)curHealth / maxHealth, 1, 1);
+	}
+	public void ChangeSword()
+	{
+		//무기 바꿔들기
+		if (swordNum >= 0)//이전에 들고있던 무기가 있다면 없애기
+		{
+			Debug.Log("swordNum=" + swordNum);
+			swordAry[swordNum].SetActive(false);
+		}
+		swordNum++;
+		swordAry[swordNum].SetActive(true);
 
-    [Header("Player Info")]
-    public int swordNum = -1; //무기 없는상태(-1)에서 시작
-    public float coin = 0;
+		//공격력 증가
+		Item newSword = swordAry[swordNum].GetComponent<Item>();
+		curSwordPower = newSword.value;
+		swordBar.localScale = new Vector3((float)curSwordPower / maxSword, 1, 1);
+	}
+	public void ChangeShield(int val)
+	{
+		curShieldPower += val;
+		if (curShieldPower > maxShield)
+			curShieldPower = maxShield;
 
-    public float curHealth = 100;
-    public float curSwordPower = 5;
-    public float curShieldPower = 2;
-
-    [SerializeField]
-    bool isDead = false;
-
-
-    /*other input*/
-    [SerializeField]
-    bool eDown;
-    [SerializeField]
-    bool leftMouseDown;
-    [SerializeField]
-    bool rightouseDown;
-
-    /*Weapons*/
-    public GameObject[] swordAry;
-
-    /*UI*/
-    public RectTransform healthBar;
-    public RectTransform swordBar;
-    public RectTransform shieldBar;
-    public Text coinText;
-
-    void Awake()
-    {
-        anim = GetComponentInChildren<Animator>();
-        rigid = GetComponent<Rigidbody>();
-        playerMovement = GetComponent<PlayerMovement>();
-
-        cam = Camera.main;
-    }
-
-    private void Start()
-    {
-        /*player info UI 세팅*/
-        ChangeCoin(0);
-        ChangeHealth(0);
-        swordBar.localScale = new Vector3((float)curSwordPower / maxSword, 1, 1);
-        shieldBar.localScale = new Vector3((float)curShieldPower / maxShield, 1, 1);
-    }
-
-    void Update()
-    {
-        GetInput();
-
-        Interaction();
-        Attack();
-    }
-
-    private void LateUpdate()
-    {
-        UpdateBarUI();
-    }
-
-    void GetInput()
-    {
-        eDown = Input.GetKeyDown("e");// KeyCode.E);
-        leftMouseDown = Input.GetButton("Fire1"); //마우스 왼쪽
-        //rightMouseDown = Input.GetButtonDown("Fire2");//마우스 우클릭
-    }
+		shieldBar.localScale = new Vector3((float)curShieldPower / maxShield, 1, 1);
+	}
 
 
-    void Interaction()
-    {
-        if (eDown && nearObj)
-        {
-            if (nearObj.tag == "Shop")
-            {
-                Shop shop = nearObj.GetComponent<Shop>();
-                shop.Enter(this);
-            }
 
-            else if (nearObj.tag == "Object") //상호작용 가능한 사물 or NPC
-            {
-                ObjectData objScript = nearObj.GetComponent<ObjectData>();
-                manager.Action(objScript);
-            }
-        }
-    }
+	// Fields : caching -----------------------------------------------------------------------------
+	private Camera cam;
+	private Animator anim;
+	private Rigidbody rigid;
+	private PlayerMovement playerMovement;
 
-    void Attack()
-    {
-        if (leftMouseDown)
-        {
-            anim.SetTrigger("doSwing");
-        }
-    }
+	// Functions ------------------------------------------------------------------------------------
+	private void interaction()
+	{
+		if (eDown && nearObj)
+		{
+			if (nearObj.tag == "Shop")
+			{
+				// 가능하면 변수선언은 'var'를 사용하시면 좋아요 by veramocor
+				var shop = nearObj.GetComponent<Shop>();
+				shop.Enter(this);
+			}
 
-    void UpdateBarUI()
-    {
-        healthBar.localScale = new Vector3((float)curHealth / maxHealth, 1, 1);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if ((other.tag == "Coin") && (!isDead))
-        {
-            Debug.Log("getcoin()");
-            Coin coinScript = other.GetComponent<Coin>();
-            ChangeCoin(coinScript.value);
-            Destroy(other.gameObject);
-        }
-    }
-
-    public void ResumePlayer()
-    {
-        playerMovement.canMove = true;
-    }
-
-    public void StopPlayer()
-    {
-        playerMovement.canMove = false;
-    }
+			else if (nearObj.tag == "Object") //상호작용 가능한 사물 or NPC
+			{
+				// GameManager를 싱글톤으로 만들었기 때문에 다음과 같은 코드도 가능하지만, 
+				// 가능하다면, 이벤트를 보내고, GameManager가 이벤트에 반응하는 식으로 하는것이 더 좋을 것 같아요.
+				var objScript = nearObj.GetComponent<ObjectData>();
+				GameManager.Instance.Action(objScript);
+			}
+		}
+	}
+	private void attack()
+	{
+		if (leftMouseDown)
+		{
+			anim.SetTrigger("doSwing");
+		}
+	}
+	private void updateBarUI()
+	{
+		healthBar.localScale = new Vector3((float)curHealth / maxHealth, 1, 1);
+	}
+	private void getInput()
+	{
+		eDown = Input.GetKeyDown("e");// KeyCode.E);
+		leftMouseDown = Input.GetButton("Fire1"); //마우스 왼쪽
+												  //rightMouseDown = Input.GetButtonDown("Fire2");//마우스 우클릭
+	}
 
 
-    public void ChangeCoin(int val)
-    {
-        coin += val;
-        coinText.text = "X " + coin;
-    }
 
-    public void ChangeHealth(int val)
-    {
-        curHealth += val;
-        if (curHealth > maxHealth)
-            curHealth = maxHealth;
+	// Unity Inspectors -----------------------------------------------------------------------------
+	[SerializeField]
+	private GameObject nearObj;//상호작용 가능한 object
+	[Header("Player Power")]
+	[SerializeField] private float maxHealth = 100;
+	[SerializeField] private int maxSword = 10;
+	[SerializeField] private int maxShield = 10;
+	[Header("Player Info")]
+	[SerializeField] private int swordNum = -1; //무기 없는상태(-1)에서 시작
+	[SerializeField] private float coin = 0;
+	[SerializeField] private float curHealth = 100;
+	[SerializeField] private float curSwordPower = 5;
+	[SerializeField] private float curShieldPower = 2;
+	[SerializeField] private bool isDead = false;
+	[Header("Other Input")]
+	[SerializeField] private bool eDown;
+	[SerializeField] private bool leftMouseDown;
+	[SerializeField] private bool rightouseDown;
+	[Header("Weapons")]
+	[SerializeField] private GameObject[] swordAry;
+	[Header("UI")]
+	[SerializeField] private RectTransform healthBar;
+	[SerializeField] private RectTransform swordBar;
+	[SerializeField] private RectTransform shieldBar;
+	[SerializeField] private Text coinText;
 
-        healthBar.localScale = new Vector3((float)curHealth / maxHealth, 1, 1);
-    }
+	// Unity Messages -------------------------------------------------------------------------------
+	private void Awake()
+	{
+		anim = GetComponentInChildren<Animator>();
+		rigid = GetComponent<Rigidbody>();
+		playerMovement = GetComponent<PlayerMovement>();
 
-    public void ChangeSword()
-    {
-        //무기 바꿔들기
-        if (swordNum >= 0)//이전에 들고있던 무기가 있다면 없애기
-        {
-            Debug.Log("swordNum=" + swordNum);
-            swordAry[swordNum].SetActive(false);
-        }
-        swordNum++;
-        swordAry[swordNum].SetActive(true);
+		cam = Camera.main;
+	}
+	private void Start()
+	{
+		/*player info UI 세팅*/
+		ChangeCoin(0);
+		ChangeHealth(0);
+		swordBar.localScale = new Vector3((float)curSwordPower / maxSword, 1, 1);
+		shieldBar.localScale = new Vector3((float)curShieldPower / maxShield, 1, 1);
+	}
+	private void Update()
+	{
+		getInput();
 
-        //공격력 증가
-        Item newSword = swordAry[swordNum].GetComponent<Item>();
-        curSwordPower = newSword.value;
-        swordBar.localScale = new Vector3((float)curSwordPower / maxSword, 1, 1);
-    }
+		interaction();
+		attack();
+	}
+	private void LateUpdate()
+	{
+		updateBarUI();
+	}
+	private void OnCollisionEnter(Collision collision)
+	{
 
-    public void ChangeShield(int val)
-    {
-        curShieldPower += val;
-        if (curShieldPower > maxShield)
-            curShieldPower = maxShield;
-
-        shieldBar.localScale = new Vector3((float)curShieldPower / maxShield, 1, 1);
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if ((other.tag == "Shop") || (other.tag == "Object"))
-        {
-            nearObj = other.gameObject;
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if ((nearObj.tag == "Shop") || (nearObj.tag == "Object"))
-        {
-            nearObj = null;
-        }
-    }
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+		if ((other.tag == "Coin") && (!isDead))
+		{
+			Debug.Log("getcoin()");
+			Coin coinScript = other.GetComponent<Coin>();
+			ChangeCoin(coinScript.value);
+			Destroy(other.gameObject);
+		}
+	}
+	private void OnTriggerStay(Collider other)
+	{
+		if ((other.tag == "Shop") || (other.tag == "Object"))
+		{
+			nearObj = other.gameObject;
+		}
+	}
+	private void OnTriggerExit(Collider other)
+	{
+		if ((nearObj.tag == "Shop") || (nearObj.tag == "Object"))
+		{
+			nearObj = null;
+		}
+	}
 }
+
+
